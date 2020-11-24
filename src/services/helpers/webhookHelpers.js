e1bc81a857ec121fcf810d8d250f0bfe926adc01
usmailaabdoul <ismaelabdul77@gmail.com>
2020-11-24 19:02:06 +0100

    jsonified response

--- src/services/helpers/webhookHelpers.js
+++ src/services/helpers/webhookHelpers.js
@@ -135,7 +135,7 @@ class WebhookHelpers {
 
     console.log(url)
     try {
-      const buttonTemplate = await fetch(url, {
+      let buttonTemplate = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
