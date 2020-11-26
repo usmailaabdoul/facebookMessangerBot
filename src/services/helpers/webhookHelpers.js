00cd09899d014d9a000ce31eabb41bf42a64bbc1
usmailaabdoul <ismaelabdul77@gmail.com>
2020-11-26 16:22:42 +0100

    cleaning up code

--- src/services/helpers/webhookHelpers.js
+++ src/services/helpers/webhookHelpers.js
@@ -49,40 +49,6 @@ class WebhookHelpers {
   }
 
   sendGenericMessage(recipientId, responseBody) {
-    const nutritionalValue = [];
-    for (let i = 0; i < responseBody.length; i++) { // I dont like using forEach
-      let obj = {
-        "title": responseBody[i].food_name,
-        "image_url": responseBody[i].thumbnail,
-        "subtitle": 'Total Calories: ' + responseBody[i].total_calories + "\n" +
-          'protein: ' + responseBody[i].protein + "\n" +
-          'Carbohydrates: ' + responseBody[i].total_carbohydrate,
-      }
-      nutritionalValue.push(obj);
-    }
-    let messageData = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": nutritionalValue
-        }
-      }
-    }
-
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: { access_token: process.env.VERIFY_TOKEN },
-      method: 'POST',
-      json: {
-        recipient: { id: recipientId },
-        message: messageData,
-      }
-    }, function (error, response, body) {
-      if (error) {
-        console.log("Error sending message: " + response.error)
-      }
-    })
   }
 
   async propertyType(recipientId) {
--- src/services/webhookEvent.js
+++ src/services/webhookEvent.js
@@ -7,56 +7,25 @@ class WebhookEvents {
   postbackEvent(event) {
     const senderID = event.sender.id;
     const payload = event.postback.payload;
-    
+
     if (payload === 'WELCOME') {
-        webHookHelper.welcomeUser(senderID);
-     }
-     else if (payload === 'APARTMENT' || payload === 'STUDIO' || payload ===  'SINGLE_ROOM' || payload === 'STORE' || payload === 'DUPLEX' || payload === 'QUEST_HOUSE') {
-        // global.property_type = payload;
-        webHookHelper.chooseLocation(senderID, payload);
-     } else if (payload === 'BUEA' || payload ===  'DOUALA' || payload ===  'LIMBE' || payload ===  'KUMBA' || payload ===  'KRIBI' || payload === 'YAOUNDE') {
-       webHookHelper.showListings(senderID, payload)
-     } else {
-       console.log(payload);
-     }
+      webHookHelper.welcomeUser(senderID);
+    }
+    else if (payload === 'APARTMENT' || payload === 'STUDIO' || payload === 'SINGLE_ROOM' || payload === 'STORE' || payload === 'DUPLEX' || payload === 'QUEST_HOUSE') {
+      // global.property_type = payload;
+      webHookHelper.chooseLocation(senderID, payload);
+    } else if (payload === 'BUEA' || payload === 'DOUALA' || payload === 'LIMBE' || payload === 'KUMBA' || payload === 'KRIBI' || payload === 'YAOUNDE') {
+      webHookHelper.showListings(senderID, payload)
+    } else {
+      console.log(payload);
+    }
   }
 
   messageEvent(event) {
-    if (!event.message.is_echo) {
-      const message = event.message;
-      const senderID = event.sender.id;
-      console.log("Received message from senderId: " + senderID);
-      console.log("Message is: " + JSON.stringify(message));
-      if (message.text) {
-        // now we will take the text recieved and send it to an food tracking API.
-        let text = message.text;
-        var request = require("request");
-
-        let options = {
-          method: 'POST',
-          url: 'https://mefit-preprod.herokuapp.com/api/getnutritionvalue',
-          headers:
-          {
-            'cache-control': 'no-cache',
-            'content-type': 'application/json'
-          },
-          body:
-          {
-            userID: process.env.USERID,
-            searchTerm: text
-          },
-          json: true
-        };
-
-        request(options, function (error, response, body) {
-          if (error) throw new Error(error);
-          webHookHelper.isTyping(senderID);
-          // webHookHelper.sendGenericMessage(senderID, body);
-          webHookHelper.welcomeUser(senderID);
-        });
-
-      }
-    }
+    const message = event.message;
+    const senderID = event.sender.id;
+    console.log("Received message from senderId: " + senderID);
+    console.log("Message is: " + JSON.stringify(message));
   }
 }
 
