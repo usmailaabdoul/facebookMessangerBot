10080d2c00e4796c5fcf2f7a717231acafd74795
usmailaabdoul <ismaelabdul77@gmail.com>
2020-12-15 11:51:12 +0100

    Adding playstore url to download app

--- src/services/helpers/postPropertyHelper.js
+++ src/services/helpers/postPropertyHelper.js
@@ -4,12 +4,56 @@ const requestType = require('../interactions/requestType');
 
 class PostPropertyHelper {
   postProperty(recipientId) {
-    const message = 'This service temporarily not available!';
+    const message = 'Post a property on DR in 3 simple steps:\n - Download the App.\n - Create a free account.\n - Post/Advertise your property(house/land).\n - Share and wait for clients in your inbox.';
 
     isTyping(recipientId);
-      sendMessage(recipientId, { text: message }).then(() => {
-        requestType(recipientId);
-      });
+    sendMessage(recipientId, { text: message }).then(() => {
+      shareDRLink(recipientId);
+    });
+  }
+
+  shareDRLink(recipientId) {
+    isTyping(recipientId);
+
+    let messageData = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "Download App",
+          buttons: [
+            {
+              type: "web_url",
+              url: 'https://play.google.com/store/apps/details?id=com.digitalrenter',
+              title: "Continue to digitalrenter.com",
+              webview_height_ratio: "full"
+            }
+          ]
+        }
+      }
+    }
+
+    const body = {
+      recipient: { id: recipientId },
+      message: messageData,
+    }
+    const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.VERIFY_TOKEN}`
+
+    try {
+      await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+
+      let message = ''
+      isTyping(recipientId);
+      // sendMessage(recipientId, { text: message }).then(() => {
+      requestType(recipientId);
+      // });
+    } catch (e) {
+      console.log("Error sending message: " + e)
+    }
   }
 }
 
